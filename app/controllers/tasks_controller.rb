f0a28cdf1ebbe3add345744665c01a487052c037
Maria Cosmen <mcosme00@gmail.com>
2023-01-07 14:16:06 +0900

    Add NEW action and bootstrap

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,4 +6,14 @@ class TasksController < ApplicationController
   def show
     @task = Task.find(params[:id])
   end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.create(params.require(:task).permit(:title, :details))
+    redirect_to tasks_path
+  end
+
 end
