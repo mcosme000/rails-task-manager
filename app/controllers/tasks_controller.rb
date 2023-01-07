c1d34a6e116b22e03245cad1703908ac29afa239
Maria Cosmen <mcosme00@gmail.com>
2023-01-07 15:00:15 +0900

    EDIT and UPDATE actions and bootstrap styles

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -13,7 +13,23 @@ class TasksController < ApplicationController
 
   def create
     @task = Task.create(params.require(:task).permit(:title, :details))
-    redirect_to tasks_path
+    redirect_to task_path(@task)
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    redirect_to task_path(@task)
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
   end
 
 end
